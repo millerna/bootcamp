d0a869bef71f85442366decc5bcb08fda95ef39c
millerna <millernaco@gmail.com>
2015-02-06 11:21:46 -0700

    site page files initialized

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bootstrap 101 Template</title>
+    <title>Bootcamp - Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic|Bree+Serif' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
@@ -13,8 +13,17 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
+  <body id="register">
+        <section class="container">
+          <div class="content row">
+             <section class="main col col-lg-8" >
 
+             </section><!-- section main -->
+             <section class="sidebar col col-lg-4">
+
+             </section><!--section sidebar-->
+          </div> <!--div content-->
+        </section><!--section container-->
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscript.js"></script>
   </body>
